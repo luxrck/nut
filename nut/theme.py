a92663ccd5de339cd56c5914bd73a14dcaf85b1c
lux.r.ck <lux.r.ck@gmail.com>
2017-04-18 23:03:47 +0800

    update default theme.

--- nut/theme.py
+++ nut/theme.py
@@ -8,9 +8,9 @@ class Theme(object):
     def __init__(self, name, root):
         self.name = name
         self.root = os.path.join(os.path.abspath(root), "themes", name)
-        rm(self.root)
-        builtin_theme_root = os.path.join(os.path.dirname(__file__), "themes")
-        cp(os.path.join(builtin_theme_root, name), self.root)
+        if not os.path.exists(self.root):
+            builtin_theme_root = os.path.join(os.path.dirname(__file__), "themes")
+            cp(os.path.join(builtin_theme_root, name), self.root)
         self.env = Environment(loader=FileSystemLoader(os.path.join(self.root, "templates")))
 
     def get_template(self, name):
