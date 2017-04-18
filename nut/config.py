c72757af8856173589b38b08ea20e0bb13127d42
lux.r.ck <lux.r.ck@gmail.com>
2017-04-18 16:28:06 +0800

    add external links support.

--- nut/config.py
+++ nut/config.py
@@ -2,6 +2,17 @@ import os
 import yaml
 from .fs import *
 
+
+def external_link(website, username):
+    symtb = {
+        "github": "https://github.com/{}",
+        "steam": "https://https://steamcommunity.com/id/{}",
+        "twitter": "https://twitter.com/{}",
+        }
+    if not website in symtb: return ""
+    return symtb[website].format(username)
+
+
 class Config(object):
     def __init__(self, root):
         self.root = os.path.abspath(root)
@@ -14,6 +25,7 @@ class Config(object):
         self.theme = "default"
         self.summary = ""
         self.output = "docs"
+        self.externals = {}
         self.articles_per_page = 10
 
     def reload(self):
@@ -21,13 +33,16 @@ class Config(object):
         if not data: return
         for k, v in data.items():
             if hasattr(self, k): setattr(self, k, v)
+        for k, v in self.externals.items():
+            self.externals[k] = external_link(k, v)
 
     def save(self):
         data = {
             "theme": self.theme,
             "summary": self.summary,
             "output": self.output,
-            "articles_per_page": self.articles_per_page
+            "articles_per_page": self.articles_per_page,
+            "externals": self.externals,
             }
         with open(self.file, "w") as stream:
             yaml.dump(data, stream, default_flow_style=False)
